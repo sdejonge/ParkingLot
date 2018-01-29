927f2decd0060944685efd196351ddb88afd46a4
Simon <s.wielenga@st.hanze.nl>
2018-01-29 10:41:24 +0100

    Met de SetupView kan nu de grote van de parkeergarage worden aangepast

--- src/SetupView.java
+++ src/SetupView.java
@@ -3,19 +3,21 @@ import java.awt.event.*;
 import javax.swing.*;
 
 
+
 public class SetupView extends JFrame {
 
     private boolean setupKlaar = false; //een boolean waarin wordt bijgehouden of de setup klaar is
+    String[] teksten =  new String[3]; //maakt een array strings om de teksten van de invoervelden in op te slaan
 
     public SetupView(){
         //Voeg een knopje toe
         JButton button = new JButton("Start Simulator");
         this.add(button,BorderLayout.NORTH);
 
-        //voeg textvelden toe (geen invoerveld)
-        JLabel label1 = new JLabel("Hello World1: ");
-        JLabel label2 = new JLabel("Hello World2: ");
-        JLabel label3 = new JLabel("Hello World3: ");
+        //voeg textvelden (labels) toe (geen invoerveld)
+        JLabel label1 = new JLabel("Floors: ");
+        JLabel label2 = new JLabel("Rows: ");
+        JLabel label3 = new JLabel("Places: ");
 
         //Voeg invoervelden toe
         JTextField tekst1 = new JTextField();
@@ -41,19 +43,16 @@ public class SetupView extends JFrame {
         //voeg de jpanelen toe aan de mainPanel
         panel1.setLayout(new BoxLayout(panel1, BoxLayout.LINE_AXIS)); //Set de layout van de jpanel
         mainPanel.add(panel1);
-        pack();
         panel2.setLayout(new BoxLayout(panel2, BoxLayout.LINE_AXIS)); //Set de layout van de jpanel
         mainPanel.add(panel2);
-        pack();
         panel3.setLayout(new BoxLayout(panel3, BoxLayout.LINE_AXIS)); //Set de layout van de jpanel
         mainPanel.add(panel3);
-        pack(); //Maak een scherm van alle velden(knoppen, tekstvelden etc)
 
         mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.PAGE_AXIS)); //Set de layout van de mainPanel
 
         //voeg de mainpanel toe aan de jframe
         this.add(mainPanel);
-        pack();
+        pack(); //Maak een scherm van alle velden(knoppen, tekstvelden etc)
         this.setTitle("Simulator-inator");
 
         //Voegt een actionlissener to aan JButton button
@@ -62,13 +61,16 @@ public class SetupView extends JFrame {
             public void actionPerformed(ActionEvent e) {
                 //setVisible(false);//sluit het setup venster
                 setupKlaar = true;
-                String B = tekst1.getText(); //leest de tekst geschreven in het tekstveld
-                System.out.println(B);
+                teksten[0] = tekst1.getText(); //leest de tekst geschreven in het tekstveld
+                teksten[1] = tekst2.getText(); //leest de tekst geschreven in het tekstveld
+                teksten[2] = tekst3.getText(); //leest de tekst geschreven in het tekstveld
+                System.out.println(teksten[0]);
                 System.out.println("Setup is klaar vanuit de actionlistener");
             }
         });
 
-        setVisible(true);
+        setSize(300, 150);
+        setVisible(true); //Opent het scherm dat net aangemaakt is
     }
 
     //Wachten totdat de gebruiker klaar is in de SetupView
@@ -79,16 +81,30 @@ public class SetupView extends JFrame {
     }
 
     public int[] setupWaardes() {
-        int[] a = {3, 6, 30}; //Geeft een aantal standaardwaardes, wordt later vervangen door invoervelden
+        int[] a = {0, 0, 0}; //Maakt een array aan met de waardes die gaan worden teruggegeven
+
+
+        a[0] = Parse(teksten[0], 3);
+        a[1] = Parse(teksten[1], 6);
+        a[2] = Parse(teksten[2], 30);
 
         return a;
     }
 
-    private void Slaap(int millisecondes){
+    private void Slaap(int millisecondes){ // een methode die een gegeven aantal millisecondes pauseerd
         try {
             Thread.sleep(millisecondes);
         } catch (InterruptedException e) {
             e.printStackTrace();
+        }
+    }
+
+    private int Parse(String s, int Reserve) {
+        try {
+            int intP = Integer.parseInt(s);
+            return intP; }
+         catch (NumberFormatException e) {
+            return Reserve;
         }
     }
 }
