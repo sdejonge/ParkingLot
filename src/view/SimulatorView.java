cc3e8f1e997f199725e40d468b1d0970a2cee6af
Stefan de Jonge <p.stefandej@outlook.com>
2018-02-02 15:17:54 +0100

    Trying to restore the master branch

--- src/view/SimulatorView.java
+++ src/view/SimulatorView.java
@@ -3,63 +3,41 @@ package view;
 import model.*;
 
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 
 public class SimulatorView extends JFrame {
 
     private CarParkView carParkView;
     private TextView textView;
+    private ProfitView profitView;
 
     private SimulatorModel model;
-    private JPanel carPanel;
-    private JPanel buttonPanel;
-
-    //    Create Jbuttons
-    public JButton start;
-    public JButton Pause;
-    public JButton honderdKeer;
-    public JButton tienKeer;
+    private JPanel topPanel;
 
     public SimulatorView(SimulatorModel model, int numberOfFloors, int numberOfRows, int numberOfPlaces) {
         this.model = model;
-
-        //Create buttons to show on JPanel
-        start = new JButton("start");
-        Pause = new JButton("Pause");
-        honderdKeer = new JButton("+100");
-        tienKeer = new JButton("+10");
-
         carParkView = new CarParkView(model, numberOfFloors, numberOfRows, numberOfPlaces);
         textView = new TextView(model);
-
-    //  Add buttons to JPanel
-        buttonPanel = new JPanel();
-        buttonPanel.add(start);
-        buttonPanel.add(Pause);
-        buttonPanel.add(honderdKeer);
-        buttonPanel.add(tienKeer);
+        profitView = new ProfitView(model);
 
         // Initiate frame
         JFrame frame = new JFrame("Parking simulator 1.0");
         this.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
         frame.setSize(900,500);
+        frame.setLayout(new BorderLayout());
 
-        carPanel = new JPanel(new BorderLayout());
-        carPanel.add(carParkView, BorderLayout.CENTER);
+        topPanel = new JPanel(new BorderLayout());
+        topPanel.setBounds(10, 10, 800, 200);
+        topPanel.setSize(800, 200);
 
-//      add panels to frame
-        frame.getContentPane().add(textView, BorderLayout.PAGE_START);
-        frame.getContentPane().add(carPanel, BorderLayout.CENTER);
-        frame.getContentPane().add(buttonPanel, BorderLayout.PAGE_END);
+        topPanel.add(textView, BorderLayout.PAGE_START);
+        topPanel.add(profitView, BorderLayout.PAGE_END);
 
-        pack();
+        frame.getContentPane().add(topPanel, BorderLayout.PAGE_START);
+        frame.getContentPane().add(carParkView, BorderLayout.CENTER);
+
+        frame.pack();
         frame.setVisible(true);
-        updateView();
     }
-
-    public void updateView() {
-        carParkView.updateView();
-        textView.updateView(model);
-    }
-
 }
